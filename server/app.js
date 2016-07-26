59620cd946992dfedf389c625de04b02bc9a20c6
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-07-25 21:31:53 -0500

    db config

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ var pg = require('pg');
 //     });
 // });
 if(process.env.DATABASE_URL != undefined) {
-    connectionString = process.env.DATABASE_URL + "?ssl=true";
+    connectionString = process.env.postgres://uxygwxoycaioiu:FsT9rQ_Z6v_Um6ViRoMWqYAdaN@ec2-50-19-227-171.compute-1.amazonaws.com:5432/d696tr2ks9oet9 + "?ssl=true";
 } else {
     // running locally, use our local database instead
     connectionString = 'postgres://localhost:5432/LJACRM';
