0eabee00c3b605ca47ba720cb23fb17cf14c30d2
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-07-30 12:58:32 -0500

    heroku push

--- modules/connection.js
+++ modules/connection.js
@@ -1,10 +1,14 @@
+ var pg = require('pg');
 // connection.js
 var connectionString = '';
 
 if(process.env.DATABASE_URL !== undefined) {
-    connectionString = process.env.DATABASE_URL + 'ssl';
-} else {
-    connectionString = 'postgres://localhost:5432/LJACRM';
-}
+     console.log('env connection string');
+     connectionString = process.env.DATABASE_URL;
+     pg.defaults.ssl = true;
+ } else {
+     connectionString = 'postgres://localhost:5432/LJACRM';
+ }
+ console.log("connectionString set to: ", connectionString);
 
 module.exports = connectionString;
